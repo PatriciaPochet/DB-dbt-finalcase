8a0850e143fa4ba0c4ea86c07df05658b1451e9b
Patricia Pochet <patricia.pochet.pro@gmail.com>
2024-11-26 13:36:41 +0000

    add schema documentation and use of stock_id in stock_coverage

--- Models/mart/mart_operations__stock_coverage.sql
+++ Models/mart/mart_operations__stock_coverage.sql
@@ -1,4 +1,5 @@
 SELECT
+    ag.stock_id,
     ag.store_id,
     ag.product_id,
     COALESCE(ag.avg_ordered_quantity_by_day, 0) AS avg_ordered_quantity_by_day,
@@ -10,7 +11,7 @@ SELECT
     cat.category_name
 FROM {{ ref('int_localbike_dataset__2018_avg_ordered_quantity' )}} AS ag
 LEFT JOIN {{ ref('stg_localbike_dataset__stock')}} AS stock
- ON ag.store_id = stock.store_id AND ag.product_id = stock.store_id
+ ON ag.stock_id = stock.stock_id 
 LEFT JOIN {{ ref('stg_localbike_dataset__store')}} AS store
  ON ag.store_id = store.store_id
 LEFT JOIN {{ ref('stg_localbike_dataset__product')}} AS pdt
